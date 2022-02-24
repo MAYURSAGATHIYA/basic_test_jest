b81b4c1c616644c96ead5d628bccdba0c7efac51
Mayur <mayursagathiya98@gmail.com>
2022-02-24 15:34:08 +0530

    basic func

---
+++ cloneArray.test.js
@@ -0,0 +1,23 @@
+const cloneArray=require('./cloneArray')
+test('ples enter properly array',()=>{
+
+    const array=[1,2,3]
+    expect(cloneArray(array)).toEqual(array)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // })const arr=[1,2,3,3,5,5,9,9,7,7]
+// const newarr=[...new Set(arr)];
+// console.log(newa)
---
+++ sum.test.js
@@ -0,0 +1,6 @@
+const sum=require('./sum')
+
+
+test('add nums proper',()=>{
+    expect(sum(1,2)).toBe(3)
+})
